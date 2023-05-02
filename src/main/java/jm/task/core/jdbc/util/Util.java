f113a9315593345c1d3fccc4a1ae18f7ef0e962b
air <hitedanil@gmail.com>
2023-05-03 00:02:46 +0300

    jdbc v2.1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -15,27 +15,8 @@ import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/java_db";
-    private static final String DB_USERNAME = "root";
-    private static final String DB_PASSWORD = "rootroot";
-
     private static SessionFactory sessionFactory;
 
-
-
-    public static Connection getConnection() {
-        Connection connection = null;
-        try {
-            Class.forName(DB_DRIVER);
-            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-            System.out.println("Connection OK");
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            System.out.println("Connection ERROR");
-        }
-        return connection;
-    }
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
             try {
